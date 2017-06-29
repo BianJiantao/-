52b30e22e158f9c405d79fe573c197cedd9731b4
BJT <BJT@bjtmacbookpro.local>
2017-06-29 15:10:11 +0800

    项目初始化

---
+++ 瀑布流布局/瀑布流布局/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  瀑布流布局
+//
+//  Created by BJT on 17/6/29.
+//  Copyright © 2017年 BJT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 瀑布流布局/瀑布流布局/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  瀑布流布局
+//
+//  Created by BJT on 17/6/29.
+//  Copyright © 2017年 BJT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
